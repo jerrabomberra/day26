62ac7716e06067e9c8b5524b6718a348f3f40c29
Doug Gordon <gordononline@bigpond.com>
2024-01-02 06:37:16 +1100

    dict update

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+# dictionaries
+
+first = {
+    "Doug": 70,
+    "Bob": 85,
+    "Vatch": 64,
+}
+
+
+# print(first.items())
+
+# for key, item in first.items():
+#     print(key, item)
+
+# print(first["Doug"])
+first["Jill"] = 87
+print(first)
+first.clear()
+print(first)
--- main.py
+++ main.py
@@ -10,12 +10,21 @@ numbers = [1, 2, 3]
 # print(new_list)
 
 
-double = [n * 2 for n in range(1, 5)]
-print(double)
+# double = [n * 2 for n in range(1, 5)]
+# print(double)
 
-list_new = [n**2 for n in range(1, 6)]
+# list_new = [n**2 for n in range(1, 6)]
 
-names = ["Joe", "Angela", "Caroline", "sebastian", "Elanor"]
+# names = ["Joe", "Angela", "Caroline", "sebastian", "Elanor"]
 
-short_names = [name.upper() for name in names if len(name) > 5]
-print(short_names)
+# short_names = [name.upper() for name in names if len(name) > 5]
+# print(short_names)
+
+# list = [9, 0, 32, 8, 2, 8, 64, 29, 42, 99]
+
+# list_of_strings = input().split(",")
+
+# numbers = [int(x) for x in list_of_strings]
+
+# result = [n for n in numbers if n % 2 == 0]
+# print(result)
