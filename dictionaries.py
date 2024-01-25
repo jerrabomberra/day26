150457e24aa84b954f0d9f6d5be17ddaffc89f28
Doug Gordon <gordononline@bigpond.com>
2024-01-25 17:28:27 +1100

    small changes

--- dictionaries.py
+++ dictionaries.py
@@ -1,57 +1,65 @@
-# import random
+# # import random
 
-# # dictionaries
+# # # dictionaries
 
-# first = {
-#     "Doug": 70,
-#     "Bob": 85,
-#     "Vatch": 64,
-# }
+# # first = {
+# #     "Doug": 70,
+# #     "Bob": 85,
+# #     "Vatch": 64,
+# # }
 
 
-# # print(first.items())
+# # # print(first.items())
 
-# # for key, item in first.items():
-# #     print(key, item)
+# # # for key, item in first.items():
+# # #     print(key, item)
 
-# # print(first["Doug"])
-# first["Jill"] = 87
-# print(first)
-# first.clear()
-# print(first)
+# # # print(first["Doug"])
+# # first["Jill"] = 87
+# # print(first)
+# # first.clear()
+# # print(first)
 
-# # new_dict = {new_key : new_value for item in list}
+# # # new_dict = {new_key : new_value for item in list}
 
-# # new_dict = {new_key: new_value for (key, value) in dict.items() if test}
+# # # new_dict = {new_key: new_value for (key, value) in dict.items() if test}
 
-# names = ["Bob", "Jill", "Jack", "Betty", " Dave", "Beth", "Alex", "Fred"]
+# # names = ["Bob", "Jill", "Jack", "Betty", " Dave", "Beth", "Alex", "Fred"]
 
-# student_scores = {student: random.randint(31, 100) for student in names}
-# print(student_scores)
+# # student_scores = {student: random.randint(31, 100) for student in names}
+# # print(student_scores)
 
-# passed_students = {
-#     student: score for (student, score) in student_scores.items() if score >= 0
-# }
-# print(passed_students)
+# # passed_students = {
+# #     student: score for (student, score) in student_scores.items() if score >= 0
+# # }
+# # print(passed_students)
 
-d = {}  # empty dictionary
+# d = {}  # empty dictionary
 
-numNames = {1: "One", 2: "Two", 3: "Three"}  # int key, string value
+# numNames = {1: "One", 2: "Two", 3: "Three"}  # int key, string value
 
-decNames = {
-    1.5: "One and Half",
-    2.5: "Two and Half",
-    3.5: "Three and Half",
-}  # float key, string value
+# decNames = {
+#     1.5: "One and Half",
+#     2.5: "Two and Half",
+#     3.5: "Three and Half",
+# }  # float key, string value
 
-items = {
-    ("Parker", "Reynolds", "Camlin"): "pen",
-    ("LG", "Whirlpool", "Samsung"): "Refrigerator",
-}  # tuple key, string value
+# items = {
+#     ("Parker", "Reynolds", "Camlin"): "pen",
+#     ("LG", "Whirlpool", "Samsung"): "Refrigerator",
+# }  # tuple key, string value
 
-romanNums = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5}  # string key,
-romanNums["VI"] = 6
-romanNums["0"] = 0
-print(romanNums)
-new_romans = {key: value for (key, value) in romanNums.items() if value % 2 == 0}
-print(new_romans)
+# romanNums = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5}  # string key,
+# romanNums["VI"] = 6
+# romanNums["0"] = 0
+# print(romanNums)
+# new_romans = {key: value for (key, value) in romanNums.items() if value % 2 == 0}
+# print(new_romans["IV"])
+
+
+pop = [30, 3, 40]
+countries = ["afgan", "albania", "algeria"]
+d1 = dict(zip(countries, pop))
+print(d1)
+
+print(d1["albania"])
